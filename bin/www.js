82ecd344cb41998d8664bc34a0d0bd599bcb1f5d
cscalcucci <cscal@aphelion-enterprises.com>
2016-03-01 20:24:00 -0500

    Fixed port issue

--- bin/www.js
+++ bin/www.js
@@ -3,8 +3,22 @@ var app = require('../app'); //Require our app
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-var server = app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port );
+function initIPAdress() {
+    var adr = process.env.OPENSHIFT_NODEJS_IP;
+
+    if (typeof adr === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using localhost');
+            adr = 'localhost';
+    }
+
+    server_ip_address = adr;
+}
+
+initIPAdress();
+
+app.listen(server_port, server_ip_address, function() {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
 });
 
 
